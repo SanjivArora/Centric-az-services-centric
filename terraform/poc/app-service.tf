58ca2e00b8abf3f049a5aca350d8e54ed984547d
Sharan Nagulapalli (healthAlliance) <Sharan.Nagulapalli@healthalliance.co.nz>
2022-11-21 09:17:28 +1300

    feat: provison pasview front end web app

--- terraform/poc/app-service.tf
+++ terraform/poc/app-service.tf
@@ -19,10 +19,47 @@ module "app-sevrice" {
   service_name                   = "poc-centric-pasview-ae-service"
   site_config = {
     minimum_tls_version = "1.2"
-    always_on           = "false"
+    always_on           = "true"
     application_stack = {
       current_stack  = "dotnet"
       dotnet_version = "v4.0"
+    }
+  }
+  app_settings = {
+    WEBSITE_HEALTHCHECK_MAXPINGFAILURES     = "10"
+    InstrumentationEngine_EXTENSION_VERSION = "disabled"
+  }
+  app_service_vnet_integration_subnet_id = data.azurerm_subnet.webapp.id
+  depends_on = [
+    azurerm_resource_group.this_rg
+  ]
+}
+
+module "app-sevrice-pasview-frontend" {
+  source = "../../modules/azure-app-service"
+
+  create_resource_group          = false
+  resource_group_name            = azurerm_resource_group.this_rg.name
+  location                       = var.location
+  create_log_analytics_workspace = false
+  log_analytics_workspace_name   = "poc-centric-ae-log-workspace"
+  log_analytics_workspace_sku    = "PerGB2018"
+  log_analytics_data_retention   = 30
+  create_service_plan            = false
+  service_plan_name              = "poc-centric-ae-service-plan"
+  os_type                        = "Windows"
+  service_plan_sku_name          = "B1"
+  create_application_insights    = false
+  application_insights_name      = "poc-centric-ae-app-insights"
+  application_insights_type      = "web"
+  application_insights_enabled   = true
+  service_name                   = "poc-centric-pasview-ae-front-end-service"
+  site_config = {
+    minimum_tls_version = "1.2"
+    always_on           = "true"
+    application_stack = {
+      current_stack  = "node"
+      node_version = "14-LTS"
     }
   }
   app_settings = {
